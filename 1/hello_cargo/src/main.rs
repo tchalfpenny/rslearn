e239cb125bf7cd3f93f211eedfaa6e7149cd2603
Tyler Halfpenny <tchalfpenny@gmail.com>
2025-03-06 21:06:51 -0500

    Chapter 1

---
+++ 1/hello_cargo/src/main.rs
@@ -0,0 +1,9 @@
+// cargo new: creates new directory and project
+// cargo init: adds/converts project to cargo, creates Cargo.toml
+// cargo build: builds project and creates executable (--release to compile with optimizations)
+// cargo run: compiles and runs executable
+// cargo check: checks that code compiles, does not produce an executable
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ 1/hello_world/main.rs
@@ -0,0 +1,8 @@
+// rustc main.rs to compile
+// ./main to run executable
+// empty parentheses, no params, returns nothing
+// ! calls a macro instead of a normal function
+
+fn main() {
+    println!("Hello, world!");
+}
